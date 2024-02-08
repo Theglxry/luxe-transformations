558903d3b11a52c6b1dd9b3fb498f2ce6693befa
Aliu Gloria <glorysamuel75@gmail.com>
2024-02-08 12:24:31 +0100

    buid components

--- app/page.tsx
+++ app/page.tsx
@@ -2,111 +2,136 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative w-full h-full">
+      <section className="relative  px-4 md:w-9/12 mt-10 md:mt-8  h-full lg:h-full m-auto z-10">
+        {/* parent div */}
+        <div className="w-full">
+          {/* hero-text */}
+          <div className="sm:w-full mt-70 md:mt-0 leading-none lg:leading-[-10rem] z-10 ">
+            <h1 className="text-9xl lg:text-[14rem] xl:text-[18rem]  -mb-2 lg:-mb-10 font-extrabold">
+              <div className="flex flex-col md:flex-row  justify-between ">
+                CREATIVE
+                <span className="text-red-500">WEB</span>
+              </div>
+            </h1>
+
+            <div className="relative flex flex-col gap-2  md:float-right z-10">
+              <h1 className=" md:relative text-9xl lg:text-[14rem] xl:text-[16rem] font-extrabold">
+                STUDIO
+              </h1>
+
+              <div className="button w-3/4 md:w-full relative font-thin flex items-center justify-between text-lg px-8 py-4 rounded-md cursor-pointer border-[1px] border-gray-500">
+                <h4>LATEST PROJECT</h4>
+                <div className="w-10 lg:w-16">
+                  <i>
+                    <svg
+                      viewBox="0 0 56 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        className="arrow-line"
+                        d="M1 6L49.8077 6"
+                        stroke="#EEEEF2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                      <path
+                        className="arrow-line"
+                        d="M1 6L49.8077 6"
+                        stroke="#EEEEF2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M49.8086 1L55.0009 6L49.8086 11L49.8086 1Z"
+                        stroke="#EEEEF2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                    </svg>
+                  </i>
+                </div>
+                <h4>O POSITIVE</h4>
+              </div>
+            </div>
+          </div>
+
+          {/* blurb cirlce */}
+          <div
+            className="absolute top-0  sm:h-[50vw] sm:w-[60vw] z-0 rounded-full bg-gradient-radial from-blue-600 via-transparent to-transparent opacity-100  lg:top-10 left-48 lg:left-0 ml-[-20rem]  "
+            style={{ opacity: 0.3 }}
+          ></div>
+
+          <div className="hidden md:flex absolute bottom-52  left-[65%] lg:left-[57%] text-gray-400 text-base">
+            {" "}
+            <h6>[ SCROLL ] </h6>
+          </div>
+
+          <div className="absolute flex md:right-0 bottom-52 text-gray-400  flex-col gap-4 w-48  lg:w-[20rem]">
+            <div className="md:hidden flex gap-6 w-full items-center">
+              <div>
+                <span className="flex absolute inset-y-0  top-1  items-center justify-center w-4 h-4 border-[1px] rounded-full">
+                  <div className=" w-2 h-2 bg-red-600  rounded-full"></div>
+                </span>{" "}
+              </div>
+
+              <h6> ABOUT</h6>
+            </div>
+
+            <span className="sm:text-2xl ">
+              WE SPECIALIZE IN CREATING MEANINGFUL DIGITAL EXPERIENCES INFUSED
+              WITH EMOTION, DRIVEN BY INNOVATION, EVOKING A SENSE OF AWE AND
+              WONDER.
+            </span>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* pencil */}
+      {/* <div className="absolute bottom-30 right-0 mb-10 mr-10 border-2 border-red-400"> */}
+      <div className="absolute top-[70%] right-28  rounded-full bg-red-500 z-10 ">
+        <svg
+          id="pencil"
+          width="80"
+          height="80"
+          viewBox="0 0 80 80"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <path
+            className="handle-a"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M12.2601 16.0778C12.1641 16.1738 12.2321 16.3379 12.3679 16.3379V16.3379C12.4073 16.3379 12.4452 16.3226 12.4736 16.2953L14.9293 13.9292L17.3586 11.4999C17.4027 11.4558 17.4215 11.3924 17.4085 11.3314V11.3314C17.3781 11.1886 17.2015 11.1364 17.0983 11.2396L12.2601 16.0778Z"
+            stroke="white"
+            stroke-width="2"
+          />
+          <path
+            className="tip-a"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M14.9293 13.9292L12.5735 15.5735L16.5735 11.5735L14.9293 13.9292Z"
+            fill="white"
+          />
+          <path
+            className="handle-b"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M27.4142 29.9237C26.6332 30.7047 26.6332 31.9711 27.4142 32.7521L43.5938 48.9316C43.8613 49.1992 44.1993 49.3854 44.5684 49.4685L50.8933 50.8933L49.4685 44.5685C49.3854 44.1993 49.1992 43.8613 48.9316 43.5938L32.7521 27.4142C31.971 26.6332 30.7047 26.6332 29.9237 27.4142L27.4142 29.9237Z"
+            stroke="white"
+            stroke-width="2"
+          />
+          <path
+            className="tip-b"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M49.7919 50.1349L45 49L49 45L49.7919 50.1349Z"
+            fill="white"
+          />
+        </svg>
       </div>
     </main>
   );
---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,101 @@
+const NavBar: React.FC = () => {
+  return (
+    <nav className="relative flex items-center justify-between p-10 h-[15vh] w-full">
+      {/* logo */}
+      <div className="absolute top-10 w-14 h-14 left-4  lg:w-16 lg:h-16">
+        <a href="/">
+          <svg
+            viewBox="0 0 60 60"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M30 60.0371C46.5652 60.0371 60 46.6117 60 30.0371C60 13.4625 46.5652 0.0371094 30 0.0371094C13.4348 0.0371094 0 13.4625 0 30.0371C0 46.6117 13.4348 60.0371 30 60.0371ZM15.1902 35.6029L19.4911 9.18965L25.8292 16.5129C27.3329 18.0928 29.548 19.3106 32.7009 19.36H35.7406C35.9831 19.3435 36.2256 19.3435 36.4519 19.3435H36.452C45.1507 19.3435 48.2713 23.6717 46.5413 34.3357C45.1346 43.0085 39.5887 48.6038 32.0541 48.8507H30.8253H25.6998C16.322 48.8507 14.3009 42.5641 15.1902 35.6029ZM36.8655 34.1049C36.2242 38.0439 33.6587 40.7151 30.4013 40.7151C27.2452 40.7151 25.4899 38.027 26.1312 34.1049C26.7726 30.1658 29.3886 27.4947 32.5448 27.4947C35.8191 27.4947 37.5069 30.1658 36.8655 34.1049Z"
+              fill="rgba(220, 38, 38)"
+            ></path>
+          </svg>
+        </a>
+      </div>
+
+      {/* center nav */}
+      <div className=" m-auto text-center md:w-full  md:ml-40 flex w-auto lg:gap-[30%] items-center md:text-lg tracking-widest text-lg  z-1 ">
+        <h6 className="hidden lg:block">BUZZWORTHY STUDIO</h6>
+
+        <div className=" relative flex items-center justify-center sm:mt-10 md:mt-0 md:gap-8 flex-col gap-2 md:flex-row  text-base md:text-lg">
+          <h4 className="relative z-10 px-4 py-2 md:px-0 md:py-1 rounded mb-2 bg-white text-black font-bold md:font-thin md:text-white md:bg-transparent ">
+            WE
+          </h4>
+          <span className="hidden md:flex absolute inset-y-0 left-6 top-2  items-center justify-center w-5 h-5 border-[1px] rounded-full">
+            <div className=" w-2 h-2 bg-red-600  rounded-full"></div>
+          </span>
+
+          {/* slides */}
+          <div className="-mt-2 ml-2">
+            <h6>TURN VISIONS INTO VALUE</h6>
+          </div>
+        </div>
+      </div>
+ 
+
+
+
+
+      {/* hamburger */}
+      <div className="right-4 md:right-20 top-10 absolute h-[5px]  lg:w-30 md:h-30">
+        <svg
+          width="30"
+          height="61"
+          viewBox="0 0 30 61"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_1490_325)">
+            <path
+              className="lineA"
+              d="M5.07 11 C5.07 27.33 5.07 43.66 5.07 60 "
+              stroke="#EEEEF2"
+              stroke-width="2"
+              data-original="M5.07849 11V60"
+            ></path>
+            <path
+              className="lineB"
+              d="M15.07 16 C15.07 25.66 15.07 35.33 15.07 45 "
+              stroke="#EEEEF2"
+              stroke-width="2"
+              data-original="M15.0785 16V45"
+            ></path>
+            <path
+              className="lineC"
+              d="M25.07 1 C25.07 17.33 25.07 33.66 25.07 50 "
+              stroke="#EEEEF2"
+              stroke-width="2"
+              data-original="M25.0784 1V50"
+            ></path>
+            <path
+              className="pointA"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M5.07 11 C6.38 10.14 7.69 9.29 9 8.44 9 6.83 9 5.22 9 3.61 7.69 2.74 6.38 1.87 5.07 1 3.74 1.87 2.41 2.74 1.07 3.61 1.05 5.22 1.02 6.83 1 8.44 2.35 9.29 3.71 10.14 5.07 11 "
+              stroke="#EEEEF2"
+              stroke-width="2"
+              data-original="M5.07843 11L9 8.4482L9 3.61273L5.07843 1L1.07843 3.61273L1 8.4482L5.07843 11Z"
+            ></path>
+            <path
+              className="pointB"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M25.07 60 C26.38 59.14 27.69 58.29 29 57.44 29 55.83 29 54.22 29 52.61 27.69 51.74 26.38 50.87 25.07 50 23.74 50.87 22.41 51.74 21.07 52.61 21.05 54.22 21.02 55.83 21 57.44 22.35 58.29 23.71 59.14 25.07 60 "
+              stroke="#EEEEF2"
+              stroke-width="2"
+              data-original="M25.0784 60L29 57.4482V52.6127L25.0784 50L21.0784 52.6127L21 57.4482L25.0784 60Z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
